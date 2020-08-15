60d1e6a8addb7f23bd4622cb83c72ee172c9de84
frederico101 <fredyalves.fredyalves@hotmail.com>
2020-08-14 22:02:55 -0300

    Implementando SharedCore e testando o metodo de limite de items inseridos

--- TDD/ToolsStore.Dominio.Test/PedidosTest.cs
+++ TDD/ToolsStore.Dominio.Test/PedidosTest.cs
@@ -5,41 +5,53 @@ using Xunit;
 
 namespace ToolsStore.Dominio.Test
 {
+    
     public class PedidosTest
     {
-        [Fact(DisplayName="Pedidos")]
+        [Fact(DisplayName = "Pedidos")]
         [Trait("Categoria", "Dominio")]
-        public void ToolsStore_Pedido_IncluirSomarOsPedidos()
+        public void AdicionarItemPedido_NovoPedido_DeveAtulizarValor()
         {
             //Arrange
-             var pedido = new Pedido();
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Serra", 2, 50);
             //Act
-            var pedidoItem = new PedidoItem(Guid.NewGuid(),"Serra",2,50);
             pedido.AdicionarPedido(pedidoItem);
             //Assert
             Assert.Equal(100, pedido.ValorTotal);
 
         }
 
-        [Fact(DisplayName="PedidosVerificaUnidades")]
+        [Fact(DisplayName = "PedidosVerificaUnidades")]
         [Trait("Categoria", "Dominio")]
-        public void ToolsStore_Pedido_DeveIncrementarESomarValores()
+        public void AdicionarItemPedido_ItemExistente_DeveIncrementarESomarValores_()
         {
             //Arrange
-             var pedido = new Pedido();
-            //Act
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
             var produtoId = Guid.NewGuid();
-            var pedidoItem = new PedidoItem(produtoId,"Serra",2,100);
+            var pedidoItem = new PedidoItem(produtoId, "Serra", 2, 100);
+            var pedidoItem2 = new PedidoItem(produtoId, "Serra", 1, 100);
+            //Act
             pedido.AdicionarPedido(pedidoItem);
-            var pedidoItem2 = new PedidoItem(produtoId,"Serra",1,100);
             pedido.AdicionarPedido(pedidoItem2);
             //Assert
-            
+
             Assert.Equal(300, pedido.ValorTotal);
             Assert.Equal(1, pedido.PedidoItems.Count());
             Assert.Equal(3, pedido.PedidoItems.FirstOrDefault(i => i.ProdutoId == produtoId).Quantidade);
 
         }
 
+        [Fact(DisplayName = "LançaException")]
+        [Trait("Categoria", "exception")]
+        public void AdicionarItemPedido_ItemAcima15Unidades_DeveRetornarExceptions()
+        {
+            //Arrange
+            var produtoId = Guid.NewGuid();
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedidoItem = new PedidoItem(produtoId, "Serra", 16, 100);
+            //Act && Assert
+            Assert.Throws<DomainException>(()=>pedido.AdicionarPedido(pedidoItem));
+        }
     }
 }
--- TDD/ToolsStore/ToolsStore.Domain/Pedido.cs
+++ TDD/ToolsStore/ToolsStore.Domain/Pedido.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,26 +7,85 @@ namespace ToolsStore.Domain
 {
     public class Pedido
     {
+         public int MAX_UNIDADES_ITEM => 15;
+
         private readonly List<PedidoItem> _pedidoItems;
-        public Pedido()
+        protected Pedido()
         {
             _pedidoItems = new List<PedidoItem>();
         }
-        public int ValorTotal { get; private set; }
-        
-        public  IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+        public Guid ClienteId { get; set; }
+        public decimal ValorTotal { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
         public void AdicionarPedido(PedidoItem pedidoItem)
         {
-              if(_pedidoItems.Any(p => p.ProdutoId == pedidoItem.ProdutoId))
-              {
-                  var itemsExistentes = _pedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId); 
-                  itemsExistentes.AdicionarUnidades(pedidoItem.Quantidade);
-                  pedidoItem = itemsExistentes;
-                  _pedidoItems.Remove(itemsExistentes);
-              }
+            if(pedidoItem.Quantidade > MAX_UNIDADES_ITEM) throw new DomainException("O maximo de item é 15 items");
+            if (_pedidoItems.Any(p => p.ProdutoId == pedidoItem.ProdutoId))
+            {
+                var itemsExistentes = _pedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId);
+                itemsExistentes.AdicionarUnidades(pedidoItem.Quantidade);
+                pedidoItem = itemsExistentes;
+                _pedidoItems.Remove(itemsExistentes);
+            }
 
             _pedidoItems.Add(pedidoItem);
+
+
+
             ValorTotal = _pedidoItems.Sum(i => i.Quantidade * i.ValorUnitario);
+
         }
+
+        public void TornarRascunho()
+        {
+            PedidoStatus = PedidoStatus.Rascunho;
+
+        }
+
+        public static class PedidoFactory
+        {
+            public static Pedido NovoPedidoRascunho(Guid clienteId)
+            {
+                var pedido = new Pedido
+                {
+                    ClienteId = clienteId,
+                };
+                pedido.TornarRascunho();
+                return pedido;
+            }
+
+        }
+
     }
+
+    public enum PedidoStatus
+    {
+        Rascunho = 0,
+        Iniciado = 1,
+        Pago = 4,
+        Entregue = 5,
+        Cancelado = 6
+    }
+
+    public class DomainException : Exception 
+    { 
+        public DomainException()
+        {
+            
+        }
+        public DomainException(string msg) : base(msg)
+        {
+            
+        }
+        public DomainException(string msg, Exception innerException) : base(msg, innerException)
+        {
+            
+        }
+        
+
+    }
+
 }
